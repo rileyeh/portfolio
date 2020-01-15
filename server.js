80ad097a73b9c034aeee8e847d4230928840d390
rileyeh <rileyehatch@gmail.com>
2020-01-15 10:40:37 -0700

    fix2 router

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-// const connect = require('connect')
-// const serveStatic = require('serve-static')
-// connect().use(serveStatic(`${__dirname}/build`)).listen(4004 , function(){
-//   console.log('Server running on 4004')
-// })
+const connect = require('connect')
+const serveStatic = require('serve-static')
+connect().use(serveStatic(`${__dirname}/build`)).listen(4004 , function(){
+  console.log('Server running on 4004')
+})
 
 
 
 
 // could do something like this to fix the problem Josh pointed out?
 
-const connect = require('connect')
-const serveStatic = require('serve-static')
-connect().use(serveStatic(`${__dirname}/public/index.html/build`)).listen(4004 , function(){
-  console.log('Server running on 4004')
-})
+// const connect = require('connect')
+// const serveStatic = require('serve-static')
+// connect().use(serveStatic(`${__dirname}/public/index.html/build`)).listen(4004 , function(){
+//   console.log('Server running on 4004')
+// })
