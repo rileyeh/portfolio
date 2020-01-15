14661e3499990db9f9d08feb23c65c4443cd22c5
rileyeh <rileyehatch@gmail.com>
2020-01-15 10:51:12 -0700

    f4

--- server.js
+++ server.js
@@ -1,26 +1,15 @@
 const connect = require('connect')
 const serveStatic = require('serve-static')
 const app = connect()
-app.use(serveStatic(`${__dirname}/build`)).listen(4004 , function(){
-  console.log('Server running on 4004')
-})
-
-
-
-
-// could do something like this to fix the problem Josh pointed out?
-
-// const connect = require('connect')
-// const serveStatic = require('serve-static')
-// connect().use(serveStatic(`${__dirname}/public/index.html/build`)).listen(4004 , function(){
-//   console.log('Server running on 4004')
-// })
-
 
 app.use('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'path/to/your/index.html'), function(err) {
+  res.sendFile(path.join(__dirname, './public/index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
     }
   })
+})
+
+app.use(serveStatic(`${__dirname}/build`)).listen(4004 , function(){
+  console.log('Server running on 4004')
 })
