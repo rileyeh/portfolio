3204cfe1d480fc7989b247688f7ef50d961d745a
rileyeh <rileyehatch@gmail.com>
2020-01-15 11:22:04 -0700

    express server working

--- server.js
+++ server.js
@@ -3,43 +3,10 @@ const path = require('path')
 
 const app = express()
 
-// app.use( express.static( `${__dirname}/../build` ) )
-// app.use( express.static( `public` ) )
 app.use(express.static(path.join(__dirname, '/build')))
 
-// app.use('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, './public/index.html'), err => {
-//     if (err) {
-//       res.status(500).send(err)
-//     }
-//   })
-// })
 app.get('*', (req,res) =>{
   res.sendFile(path.join(__dirname+'/build/index.html'))
 })
 
 app.listen(4004, () => console.log(`up and running on 4004`))
-
-
-
-
-
-
-
-
-
-// const connect = require('connect')
-// const serveStatic = require('serve-static')
-// const app = connect()
-
-// app.use('/*', function(req, res) {
-//   res.sendFile(path.join(__dirname, './public/index.html'), function(err) {
-//     if (err) {
-//       res.status(500).send(err)
-//     }
-//   })
-// })
-
-// app.use(serveStatic(`${__dirname}/build`)).listen(4004 , function(){
-//   console.log('Server running on 4004')
-// })
