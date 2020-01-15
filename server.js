da968ba1e1c6a7f3d6e382e400e286269be04e70
rileyeh <rileyehatch@gmail.com>
2020-01-15 11:18:27 -0700

    express fix2

--- server.js
+++ server.js
@@ -1,3 +1,33 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+// app.use( express.static( `${__dirname}/../build` ) )
+// app.use( express.static( `public` ) )
+app.use(express.static(path.join(__dirname, 'client/build')))
+
+// app.use('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, './public/index.html'), err => {
+//     if (err) {
+//       res.status(500).send(err)
+//     }
+//   })
+// })
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/client/build/index.html'))
+})
+
+app.listen(4004, () => console.log(`up and running on 4004`))
+
+
+
+
+
+
+
+
+
 // const connect = require('connect')
 // const serveStatic = require('serve-static')
 // const app = connect()
@@ -13,22 +43,3 @@
 // app.use(serveStatic(`${__dirname}/build`)).listen(4004 , function(){
 //   console.log('Server running on 4004')
 // })
-
-const express = require('express')
-const path = require('path')
-
-const app = express()
-
-app.use('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'), err => {
-    if (err) {
-      res.status(500).send(err)
-    }
-  })
-})
-
-app.use( express.static( `${__dirname}/../build` ) )
-
-app.listen(4004, () => console.log(`up and running on 4004`))
-
-
