2826b3d88e6983914e3606e9ccea372c57267dd8
rileyeh <rileyehatch@gmail.com>
2020-01-20 10:49:55 -0700

    lecture 2

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -15,7 +15,7 @@ export default function Home() {
           <img src={riley} alt=''/>
           <Text>
           {/* I am a full stack web developer who specializes in JavaScript, ReactJS, HTML, CSS, NodeJS, and PostgreSQL. I enjoy teaching, learning, and problem solving. Outside of work, you can find me with my family and friends, outside with my dog, or with a book. I also enjoy reading, playing games, hand lettering, and drawing. I love web development because it combines logic and design. */}
-          DevMountain
+          DevMountain, more text
           </Text>
         </Main>
       </Card>
